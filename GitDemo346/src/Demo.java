54f1fb1c7efba513190581f4c11c97a133d539a9
MANISH <MANISH@DESKTOP-NPV50CU>
2022-09-22 15:20:19 +0530

    jjcjc

---
+++ GitDemo346/src/Demo.java
@@ -0,0 +1,50 @@
+
+public class Demo 
+{
+	
+	
+    
+public void m1()
+{
+System.out.println(" hello m1 method ");
+}
+
+public void m2()
+{
+System.out.println(" hello m2 method ");
+}
+
+public void m3()
+{
+System.out.println(" hello m3 method ");
+}
+
+public void sum()
+{
+
+System.out.println(" hello sum method ");
+}
+
+public void mul()
+{
+System.out.println(" hello mul method ");
+}
+
+public static void main(String[] args)
+{
+
+
+Demo a=new Demo();
+
+a.m1();
+a.m2();
+a.m3();
+a.sum();
+a.mul();
+
+
+}
+
+
+
+}
